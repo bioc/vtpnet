e669e29a10d23f8ff4bf7c5c3c18aa1e18331c6d
Vince Carey <stvjc@channing.harvard.edu>
2013-06-02 21:03:25 +0000

    introduce matTx to permit scoring of the reverse complement strand
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/vtpnet@77098 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/unipsearch.R
+++ R/unipsearch.R
@@ -2,13 +2,19 @@ setupGWPM = function(cores=11) {
    require(foreach)
    require(doParallel)
    registerDoParallel(cores=14)
-   require(BSgenome.Hsapiens.UCSC.hg19)
    require(MotifDb)
 }
 
-gwMatchPWM = function( mdb1, tag, bsg=Hsapiens, nchr=24, thresh="75%" ) {
+matrc = function(x) {
+  x[4:1, ncol(x):1]
+}
+
+gwMatchPWM = function( mdb1, tag, bsg=Hsapiens, nchr=24, thresh="75%", matTx=force ) {
 #
 # assumes you only want to process sequences 1:nchr in bsg
+# bsg is any object answering seqnames and [[ seqname[i] ]] wih DNAString
+#
+# use matTx=matrc to match the reverse complement model
 #
     if (!is(mdb1, "MotifList")) stop("operates only on MotifList instances")
     if (length(mdb1)>1) stop("input must be of MotifList of length 1")
@@ -17,8 +23,8 @@ gwMatchPWM = function( mdb1, tag, bsg=Hsapiens, nchr=24, thresh="75%" ) {
     ans = foreach(i=1:nchr) %dopar% {
 #     cat(i)
      subj = bsg[[ sn[i] ]]
-     hits = matchPWM( mdb1[[1]], subj, thresh )
-     scores = PWMscoreStartingAt( mdb1[[1]], subject(hits), start(hits) )
+     hits = matchPWM( matTx(mdb1[[1]]), subj, thresh )
+     scores = PWMscoreStartingAt( matTx(mdb1[[1]]), subject(hits), start(hits) )
      GRanges( seqnames=sn[i], ranges=as(hits, "IRanges"), tag=rep(tag,length(hits)),
          score=scores)
     }
@@ -32,7 +38,7 @@ getMtags = function(mdb, fixer=function(x) gsub("\\/", "_", x)) {
    list(utags = fixer(utags), longtags = fixer(longtags))
 }
 
-MdbSearchToDisk = function(mdb, thresh="80%", cores=14) {
+MdbSearchToDisk = function(mdb, bsg, thresh="80%", cores=14, ...) {
    setupGWPM(cores=cores)
    tagSet = getMtags(mdb)
    obn = tagSet$utags
@@ -40,7 +46,7 @@ MdbSearchToDisk = function(mdb, thresh="80%", cores=14) {
   
    for (i in 1:length(obn)) {
 #    cat(i)
-    assign( obn[i], gwMatchPWM( mdb[i], obn[i], thresh=thresh ) )
+    assign( obn[i], gwMatchPWM( mdb1=mdb[i], tag=obn[i], bsg=bsg, thresh=thresh, ... ) )
     save(list=obn[i], file=fn[i] )
     gc()
    }
